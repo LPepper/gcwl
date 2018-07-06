f8a8103156a37fab24bca8959719677ab7ed47a1
LPepper <437413764@qq.com>
2018-07-06 10:14:56 +0800

    开发环境配置

---
+++ src/test/java/com/spas/gcwl/GcwlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spas.gcwl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GcwlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
